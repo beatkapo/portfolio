9503c1b97f838528e734d090d6724d3a30234361
Fran Gabarda <frangabardaortiz@gmail.com>
2024-06-04 11:50:57 +0200

    Languages
    Implementada la opción de cambiar de idioma la página

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,43 @@ const toggleColors = document.getElementById('toggle-colors');
 
 const rootStyles = document.documentElement.style;
 
+const flagsElement = document.getElementById('flags');
+
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const bromeraImage = document.getElementById('bromera-image');
+
+const changueLanguage = async language=>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML= texts[section][value];
+    }
+    if(toggleIcon.src.includes('moon.svg')) {
+        toggleText.textContent = texts['mode']['dark'];
+    }else{
+        toggleText.textContent = texts['mode']['light'];
+    }
+}
+
+flagsElement.addEventListener('click', (e) => {
+    changueLanguage(e.target.parentElement.dataset.language);
+});
+
 toggleTheme.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     if(toggleIcon.src.includes('moon.svg')) {
         toggleIcon.src = 'assets/icons/sun.svg';
         toggleText.textContent = 'Light Mode';
+        bromeraImage.src = 'assets/images/logo-bromera.png';
     }else{
         toggleIcon.src = 'assets/icons/moon.svg';
         toggleText.textContent = 'Dark Mode';
+        bromeraImage.src = 'assets/images/logo-bromera-dark.png';
     }
 });
 
